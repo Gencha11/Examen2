f094cafcded51d527b07a532e59e0041e09662be
GENESIS DAYANNA CHAVARRIA <gchavarriat0654@uvirtual.ac.cr>
2021-07-23 23:28:29 -0600

    Add project files.

---
+++ BaseDatos/dbo/StoreProcedures/DireccionObtener.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE [dbo].DireccionObtener
+@IdDireccion INT=NULL
+
+AS BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+
+	        CP.IdCatalogoProvincia
+		,	CP.NombreCatalogoProvincia
+
+		,	D.IdDireccion
+		,   D.DireccionExacta
+		,   D.Estado
+
+		,   CD.IdCatalogoDistrito
+		,	CD.NombreCatalogoDistrito
+	
+		,   CC.IdCatalogoCanton
+		,	CC.NombreCatalogoCanton
+		
+
+		,   P.IdPersona
+		,	P.Nombre
+	
+				
+
+	FROM dbo.Direccion D
+	 INNER JOIN dbo.CatalogoProvincia CP
+         ON D.IdCatalogoProvincia = CP.IdCatalogoProvincia
+     INNER JOIN dbo.CatalogoCanton CC
+         ON D.IdCatalogoCanton = CC.IdCatalogoCanton
+	 INNER JOIN dbo.CatalogoDistrito CD
+         ON D.IdCatalogoDistrito = CD.IdCatalogoDistrito
+	 INNER JOIN dbo.Persona P
+         ON D.IdPersona = P.IdPersona
+	WHERE
+	     (@IdDireccion IS NULL OR IdDireccion=@IdDireccion)
+
+END
---
+++ BaseDatos/dbo/StoreProcedures/PersonaInsertar.sql
@@ -0,0 +1,63 @@
+﻿CREATE PROCEDURE dbo.PersonaInsertar
+    @IdPersona INT,
+	@Cedula varchar(250)	,
+	@Nombre varchar(250)	,
+	@Primer_Apellido varchar(250),
+	@Segundo_Apellido varchar(250),
+	@FechaNacimiento DATE,
+	@TieneHijos BIT ,
+	@Cuantos VARCHAR(1000)= NULL,
+	@Estado BIT 
+	
+AS BEGIN
+SET NOCOUNT ON
+
+	BEGIN TRANSACTION TRASA
+
+	BEGIN TRY
+	-- AQUI VA EL CODIGO
+		
+		INSERT INTO dbo.Persona 
+		(
+		  IdPersona
+	    , Cedula
+	    , Nombre 
+	    , Primer_Apellido
+	    , Segundo_Apellido 
+	    , FechaNacimiento 
+		, TieneHijos
+		, Cuantos
+		, Estado
+		)
+		VALUES
+		(
+		 @IdPersona
+		, @Cedula
+	    , @Nombre 
+	    , @Primer_Apellido
+	    , @Segundo_Apellido 
+	    , @FechaNacimiento 
+		, @TieneHijos
+		, @Cuantos
+		, @Estado
+		)
+
+
+		COMMIT TRANSACTION TRASA
+		
+		SELECT 0 AS CodeError, '' AS MsgError
+
+
+
+	END TRY
+
+	BEGIN CATCH
+		SELECT 
+				ERROR_NUMBER() AS CodeError
+			,	ERROR_MESSAGE() AS MsgError
+
+			ROLLBACK TRANSACTION TRASA
+	END CATCH
+
+
+END
